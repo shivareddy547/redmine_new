62ced0a264f7b3ea2d409e7de11092a3a76ab810
shivareddys <shiva.srinivasreddy@object-frontier.com>
2015-10-08 18:20:05 +0530

    commit js

--- plugins/employee_info/assets/javascripts/employee_info.js
+++ plugins/employee_info/assets/javascripts/employee_info.js
@@ -2,9 +2,7 @@
 $(document).on('click', '#user_billable', function() {
     // code here
     member_id = $(this).attr("member_id");
-
-
-    if($(this).is(":checked")) {
+   if($(this).is(":checked")) {
         var billable_status= true
         $('<input>').attr({
             type: 'hidden',
@@ -13,10 +11,6 @@ $(document).on('click', '#user_billable', function() {
             value: billable_status
         }).appendTo('#member-'+member_id+'-roles-form');
 
-
-//             <input id="member_billable_6826" name="billable" type="hidden" value="true">
-
-//        $("#member_billable_"+member_id).val("true")
     }
     else
     {
@@ -27,16 +21,17 @@ $(document).on('click', '#user_billable', function() {
             name: 'billable',
             value: billable_status
         }).appendTo('#member-'+member_id+'-roles-form');
-//        $("#member_billable_"+member_id).val("false")
+
     }
 
 });
 
 $(document).on('click', 'table.members .icon-edit', function() {
     $(this).closest('tr').find("#user_billable").attr("disabled", false);
-
     var billable_status = $(this).closest('tr').find("#member_billable_status").val();
     var member_id = $(this).closest('tr').find("#member_billable_status").attr("member_id");
+    $('#member-'+member_id+'-roles-form').find('a').attr('id', 'cancel_member');
+    $('#member-'+member_id+'-roles-form').find('a').attr('member_id', member_id);
     console.log(billable_status);
     console.log(member_id);
 
@@ -47,5 +42,25 @@ $(document).on('click', 'table.members .icon-edit', function() {
         value: billable_status
     }).appendTo('#member-'+member_id+'-roles-form');
 
+
+
 });
 
+$(document).on('click', '#cancel_member', function() {
+    var billable_status = $(this).closest('tr').find("#member_billable_status").val();
+    //alert(billable_status);
+    var member_id = $(this).attr("member_id");
+    if(billable_status=="true")
+    {
+
+        $(this).closest('tr').find("#user_billable").prop('checked', true);
+    }
+    else{
+
+        $(this).closest('tr').find("#user_billable").prop('checked', false);
+    }
+
+    $(this).closest('tr').find("#user_billable").attr("disabled", true);
+    return false;
+
+});
